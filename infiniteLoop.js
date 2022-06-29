d79717a956db729302b1a34d3ddab1b001ad0832
Kevin Bell <bellKevin@pm.me>
2022-06-29 18:47:09 +0000

    Create infiniteLoop.js

---
+++ infiniteLoop.js
@@ -0,0 +1,23 @@
+function infiniteLoop(arr,d,n){
+  var t=[].concat(...arr)
+  var c=0, l=t.length
+  for(var i=0;i<arr.length;i++){
+      for(var j=0;j<arr[i].length;j++){
+          if (d=='left'){
+              if (c<Math.abs(l-n))
+                arr[i][j]=t[c+n]
+              else
+                arr[i][j]=t[Math.abs(l-c-n)]
+              c++
+            }
+          else{
+              if (c<n)
+                arr[i][j]=t[l+c-n]
+              else
+                arr[i][j]=t[c-n]
+              c++
+            }
+        }
+    }
+  return arr
+}
